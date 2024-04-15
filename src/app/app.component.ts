16739fad679cc55aa623a74760874fa917cbd5a5
Udhayakumar S <86413988+Udhayakumar01@users.noreply.github.com>
2024-04-16 00:49:43 +0530

    Data Binding
    Types of Data Binding in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,17 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  //template: '<h1>My first angular app from inline</h1>',
-  //styleUrl: './app.component.scss',
-  styles: ['h1{color:red;}']
+  //template: '<h1>My first angular app from inline</h1>', -- inline HTML Template
+  styleUrl: './app.component.scss',
+  //styles: ['h1{color:red;}'] -- inline Style
 })
 export class AppComponent {
   title = 'RoomInventoryApp';
+  numberOfRooms = 10;
+  hideRooms = true;
+
+  toggle()
+  {
+      this.hideRooms = !this.hideRooms;
+  }
 }
